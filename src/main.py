d0c2ab6c055b55074da41f198f057cabafc9bb9f
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-20 10:24:42 -0500

    start writing getVerbsOfLine

--- src/main.py
+++ src/main.py
@@ -12,9 +12,17 @@ class CommandType(Enum):
 class LiteralType(Enum):
     String = auto()
     Integer = auto()
-    
+
+CHUNK_KIND_TYPE_MAP = {
+    "prefixes": {
+        "%": CommandType.GlobalSet,
+        "!": CommandType.Run,
+        "$": CommandType.ShellRun
+    }
+}
+
 class Verb():
-    def __init__ (self, kind: VerbKind, kindType: LiteralType|CommandType, literalValue: str|int|None):
+    def __init__ (self, kind: VerbKind, kindType: LiteralType|CommandType, literalValue: str|int|None = None):
         self.kind = kind
         self.kindType = kindType
 
@@ -35,8 +43,21 @@ class Parser:
         return [line for line in lines if len(line) != 0]
 
     @staticmethod
-    def getLineVerbs(line):
-        """"""
+    def getVerbsOfLine(line):
+        chunks = line.split()
+        verbs: list[Verb] = []
+
+        isFirstChunk = True
+        isInBlock = False
+
+        for chunk in chunks:
+            verb: Verb
+            if (isFirstChunk):
+                if (chunk in CHUNK_KIND_TYPE_MAP["prefixes"]):
+                    verb = Verb(VerbKind.Command, CHUNK_KIND_TYPE_MAP["prefixes"][chunk])
+                isFirstChunk = False
+
+
         
 
 code = """
